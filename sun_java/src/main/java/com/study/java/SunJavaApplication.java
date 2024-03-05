a8d44f67590f599a0dafc627806c9d5ad11fd840
sun <sunchenglong0416@163.com>
2024-03-05 21:35:39 +0800

    commit message

---
+++ sun_java/src/main/java/com/study/java/SunJavaApplication.java
@@ -0,0 +1,13 @@
+package com.study.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunJavaApplication.class, args);
+    }
+
+}
---
+++ sun_java/src/main/java/com/study/java/jvm/jvm-03-生产调优.md
@@ -0,0 +1,2 @@
+* [内存结构]() 
+* [垃圾回收](https://blog.csdn.net/goldenfish1919/article/details/82911948)
